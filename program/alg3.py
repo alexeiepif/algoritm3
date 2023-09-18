7e92c31be99c569a80fdf4798c74bb9cf81538c7
alexeiepif <aled2093746@gmail.com>
2023-09-18 18:06:26 +0300

    добавление коэффицента корелляции

--- program/alg3.py
+++ program/alg3.py
@@ -1,4 +1,5 @@
 import random as rnd
+from statistics import correlation
 import matplotlib.pyplot as plt
 import numpy as np
 import timeit
@@ -15,17 +16,20 @@ def create_graph(b, c, aur, bur):
     y_values = np.linspace(0, max(c), num=5)
     x_values = np.linspace(0, b[-1], num=11)
     plt.scatter(b, c, s=5)
-
-    y_line = aur * x_values + bur
-    plt.plot(x_values, y_line, color='red')
+    y_line = aur * np.linspace(b[0], b[-1], num=len(b)) + bur
+    plt.plot(b, y_line, color='red')
 
     plt.title("График")
     plt.xlabel("X-ось")
     plt.ylabel("Y-ось")
     plt.xticks(x_values)
     plt.yticks(y_values)
+    correlation_coefficient = np.corrcoef(c, y_line)[0, 1]
+    return correlation_coefficient
 
-#Цикл нужен для создания двух графиков, один при среднем случае, второй при худшем
+
+correlation = []
+# Цикл нужен для создания двух графиков, один при среднем случае, второй при худшем
 for iter in [1, 2]:
     x = []
     time = []
@@ -35,7 +39,7 @@ for iter in [1, 2]:
         x.append(i)
         a = [rnd.randint(1, 100) for j in range(i)]
         if iter == 1:
-            b = a[rnd.randint(1, len(a))]
+            b = a[rnd.randint(1, len(a)-1)]
         else:
             b = 101
         timer = timeit.timeit(lambda: find(a, b, i), number=1)
@@ -57,18 +61,22 @@ for iter in [1, 2]:
     sx2 = sum(x2)
     sxtime = sum(xtime)
     n = len(x)
-    # Начало вычислений коэффицентов прямой, 
-    # k - это такой коэффицент, при вычитании второго уравнения, 
-    # умноженного на него, из первого уравнения, коэффицент при x исчезает, 
+    # Начало вычислений коэффицентов прямой,
+    # k - это такой коэффицент, при вычитании второго уравнения,
+    # умноженного на него, из первого уравнения, коэффицент при x исчезает,
     # и мы находим свободный коэффицент
     k = sx2/sx
-    #bur - это свободный коэффицент
+    # bur - это свободный коэффицент
     bur = (sxtime - k*stime)/(sx-k*n)
-    #aur - это коэффицент при x
+    # aur - это коэффицент при x
     aur = (stime - bur*n)/sx
-    #Создание графических окон
+    # Создание графических окон
     plt.figure(iter)
-    #Создание графиков
-    create_graph(x, time, aur, bur)
-#Показ графиков
+    # Создание графиков
+    correlation.append(create_graph(x, time, aur, bur))
+
+print("Коэффициент корреляции в первом случае =",
+      correlation[0], "\nа во втором случае =", correlation[1])
+
+# Показ графиков
 plt.show()
